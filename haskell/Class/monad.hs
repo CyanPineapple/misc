ece31f9849b17a3e4ebd2186fd630d3a4fc2a67c
PoloGe <119010079@link.cuhk.edu.cn>
2022-09-29 15:38:42 +0800

    feat: add monad app
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

--- haskell/Class/monad.hs
+++ haskell/Class/monad.hs
@@ -1,4 +1,22 @@
-applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+--applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+--applyMaybe Nothing _ = Nothing
+--applyMaybe (Just a) f = f a
 
-applyMaybe Nothing _ = Nothing
-applyMaybe (Just a) f = f a
+
+--  return (0,0) >>= landLeft 3 >>= landRight 5
+
+
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+
+landLeft :: Birds -> Pole -> Maybe Pole  
+landLeft n (left,right)  
+    | abs ((left + n) - right) < 4 = Just (left + n, right)  
+    | otherwise                    = Nothing  
+  
+landRight :: Birds -> Pole -> Maybe Pole  
+landRight n (left,right)  
+    | abs (left - (right + n)) < 4 = Just (left, right + n)  
+    | otherwise                    = Nothing
