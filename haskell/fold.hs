1bcd0efd4326ed2d12e5b7fb9553bab8a6059480
PoloGe <119010079@link.cuhk.edu.cn>
2023-05-01 18:35:28 +0800

    feat: add another case for ffoldr
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

--- haskell/fold.hs
+++ haskell/fold.hs
@@ -14,4 +14,14 @@ ffoldr :: (b -> a -> b) -> b -> [a] -> b
 ffoldr _ acc [] = acc
 ffoldr f acc (x: xs) = f (ffoldr f acc xs) x
 
+-- write ffoldr :: (a->b->b) -> b -> t a -> b
+-- write the process out helps you write the def
+-- e.g foldr (+) 0 [1, 2, 3, 4]
+-- ==  foldr (+) 0 [1, 2, 3, 4]
+-- ==  ...
+-- ==  1 + (2 + (3 + (4 + (foldr (+) 0 []))))
 
+ffoldr _ acc [] = acc
+
+ffoldr f acc [x: xs] = 
+	f xs (ffoldr f acc [xs])
