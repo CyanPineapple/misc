b097729c6a83e6e1e0d7b93fc828810dd0aded06
PoloGe <119010079@link.cuhk.edu.cn>
2023-06-30 22:25:00 +0800

    feat: add memoization for fibo
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

--- clojure/fibo.clj
+++ clojure/fibo.clj
@@ -4,4 +4,14 @@
                       (recur two (+ one two) (inc n))))]
                    (fibo_recur 1 2 3)))
 
-(fibo 5)
+(time (fibo 50))
+
+
+(def fibo-m 
+  ( memoize (fn [iter] (let [fibo_recur(fn [one two n]
+                   (if (= n iter)
+                      two
+                      (recur two (+ one two) (inc n))))]
+                   (fibo_recur 1 2 3)))))
+
+(time (fibo-m 50))
